a2b345e1b00b8bc2939c273f9bf0875009ad414d
areagein <75215412+areagein@users.noreply.github.com>
2023-08-10 19:49:10 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,63 @@
-const glados = async () => {
-  const cookie = process.env.GLADOS
-  if (!cookie) return
-  try {
-    const headers = {
-      'cookie': cookie,
-      'referer': 'https://glados.rocks/console/checkin',
-      'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36',
-    }
-    const checkin = await fetch('https://glados.rocks/api/user/checkin', {
-      method: 'POST',
-      headers: { ...headers, 'content-type': 'application/json' },
-      body: '{"token":"glados.network"}',
-    }).then((r) => r.json())
-    const status = await fetch('https://glados.rocks/api/user/status', {
-      method: 'GET',
-      headers,
-    }).then((r) => r.json())
-    return [
-      'Checkin OK',
-      `${checkin.message}`,
-      `Left Days ${Number(status.data.leftDays)}`,
-    ]
-  } catch (error) {
-    return [
-      'Checkin Error',
-      `${error}`,
-      `<${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}>`,
-    ]
-  }
-}
+import json
+import os
 
-const notify = async (contents) => {
-  const token = process.env.NOTIFY
-  if (!token || !contents) return
-  await fetch(`https://www.pushplus.plus/send`, {
-    method: 'POST',
-    headers: { 'content-type': 'application/json' },
-    body: JSON.stringify({
-      token,
-      title: contents[0],
-      content: contents.join('<br>'),
-      template: 'markdown',
-    }),
-  })
-}
+import requests
 
-const main = async () => {
-  await notify(await glados())
-}
+from dailycheckin import CheckIn
 
-main()
+
+class Glados(CheckIn):
+    name = "GLADOS"
+
+    def __init__(self, check_item):
+        self.check_item = check_item
+
+    @staticmethod
+    def sign(cookies):
+        checkin_url = "https://glados.rocks/api/user/checkin"
+        status_url = "https://glados.rocks/api/user/status"
+        payload = {
+            'token': 'glados.network'
+        }
+        headers = {'cookie': cookies,
+                   'referer': "https://glados.rocks/console/checkin",
+                   'origin': "https://glados.rocks",
+                   'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36",
+                   'content-type': 'application/json;charset=UTF-8'}
+        result = "签到失败"
+        email = "无法获取"
+        leftDays = "无法获取"
+        message = "无法获取"
+        try:
+            checkin_res = requests.post(
+                checkin_url, headers=headers, data=json.dumps(payload)).json()
+            status = requests.get(status_url, headers=headers).json()
+            message = checkin_res['message']
+            if 'list' in checkin_res:
+                result = "签到成功"
+                email = status['data']['email']
+                leftDays = status['data']['leftDays'].split('.')[0]
+        except Exception as errorMsg:
+            result = "签到异常"
+            message = repr(errorMsg)
+        return [
+            {"name": "帐号信息", "value": email},
+            {"name": "签到结果", "value": result},
+            {"name": "剩余天数", "value": leftDays},
+            {"name": "签到信息", "value": message},
+        ]
+
+    def main(self):
+        cookie = self.check_item.get("cookie")
+        msg = self.sign(cookie)
+        msg = "\n".join(
+            [f"{one.get('name')}: {one.get('value')}" for one in msg])
+        print(msg)
+        return msg
+
+
+if __name__ == "__main__":
+    with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json"), "r", encoding="utf-8") as f:
+        datas = json.loads(f.read())
+    _check_item = datas.get("GLADOS", [])[0]
+    print(Glados(check_item=_check_item).main())
